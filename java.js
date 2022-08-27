95c59416a9cf69c05872d6bf5ad2951126684d98
ljs294 <ljsmall44@gmail.com>
2022-08-27 14:10:11 -0700

    Finishes console RPS game.

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    let rand = math.random();
+    let rand = Math.random();
     let choice;
     if (rand <= 0.33) {
         choice = 'Rock';
@@ -8,4 +8,53 @@ function getComputerChoice() {
     } else {
         choice = 'Scissors';
     }
+
+    return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = capitalize(playerSelection);
+    computerSelection = capitalize(computerSelection);
+    let message;
+    let winner = 2;
+
+    if (playerSelection === computerSelection) {
+        message = 'It\'s a draw!';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper' ||
+        playerSelection === 'paper' && computerSelection === 'scissors' ||
+        playerSelection === 'scissors' && computerSelection === 'rock') {
+        message = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        winner = 0;
+    } else {
+        message = `You Win! ${playerSelection} beats ${computerSelection}`;
+        winner = 1;
+    }
+    return [message,winner];
+}
+
+function game() {
+    let compScore = 0;
+    let playerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        result = playRound(prompt('Your Pick?'),getComputerChoice())
+        console.log(result[0]);
+        if (result[1] === 1) {
+            playerScore++;
+        } else if (result[1] === 0) {
+            compScore++;
+        }
+
+        console.log(`Score is Player: ${playerScore}, Computer: ${compScore}`);
+    }
+
+    return winner;
+}
+
+function capitalize(string) {
+    string = string.toLowerCase();
+    string = string.replace(string.charAt(0),string.charAt(0).toUpperCase());
+    return string;
+}
+
+console.log(game());
